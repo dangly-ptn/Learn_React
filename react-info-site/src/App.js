8c6a05901c4203da4764fbf468f784f542cedef2
DangLyPTN <dang.ly@ptnglobalcorp.com>
2021-12-16 20:14:06 +0700

    Finish meme-generator, Add react-info-site darkmode

--- react-info-site/src/App.js
+++ react-info-site/src/App.js
@@ -1,12 +1,21 @@
-import React from "react";
-import NavBar from "./components/NavBar";
-import Main from "./components/Main";
+import React from "react"
+import NavBar from "./components/NavBar"
+import Main from "./components/Main"
 
 export default function App() {
+  const [darkMode, setDarkMode] = React.useState(false)
+
+  function toggleDarkMode() {
+    setDarkMode(prevMode => !prevMode)
+  }
+
   return (
     <div className="container">
-      <NavBar />
-      <Main />
+      <NavBar
+        darkMode={darkMode}
+        toggleDarkMode={toggleDarkMode}
+      />
+      <Main darkMode={darkMode} />
     </div>
   )
 }
--- react-info-site/src/components/NavBar.js
+++ react-info-site/src/components/NavBar.js
@@ -1,12 +1,30 @@
-import React from "react";
-import react_icon_small from '../images/react-icon-small.png';
+import React from "react"
+import react_icon_small from '../images/react-icon-small.png'
 
-export default function NavBar() {
+export default function Navbar(props) {
   return (
-    <nav>
-      <img src={react_icon_small} alt="" className="nav--icon" />
+    <nav
+      className={props.darkMode ? "dark" : ""}
+    >
+      <img
+        className="nav--logo_icon"
+        src={react_icon_small}
+        alt=""
+      />
       <h3 className="nav--logo_text">ReactFacts</h3>
-      <h4 className="nav--title">React Course - Project 1</h4>
+
+      <div
+        className="toggler"
+      >
+        <p className="toggler--light">Light</p>
+        <div
+          className="toggler--slider"
+          onClick={props.toggleDarkMode}
+        >
+          <div className="toggler--slider--circle"></div>
+        </div>
+        <p className="toggler--dark">Dark</p>
+      </div>
     </nav>
   )
 }
